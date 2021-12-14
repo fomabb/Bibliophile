cce2be66f2d5b92351cdc029f00ab1e6f47077a1
fomabb <fomabb@yandex.by>
2021-12-14 23:48:58 +0300

    create constant

--- src/main/java/by/overone/bibliophile/util/constant/UserConstant.java
+++ src/main/java/by/overone/bibliophile/util/constant/UserConstant.java
@@ -1,4 +1,8 @@
 package by.overone.bibliophile.util.constant;
 
 public class UserConstant {
+    public static String ID = "user_id";
+    public static String LOGIN = "login";
+    public static String PASSWORD = "password";
+    public static String EMAIL = "email";
 }
